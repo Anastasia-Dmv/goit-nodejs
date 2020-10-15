a604d3689f35b5d7e7f649891b2d7d172d6e0b44
Anastasia-Dmv <Vesta50000@gmail.com>
2020-10-15 13:25:29 +0300

    hof

--- contacts.js
+++ contacts.js
@@ -5,7 +5,40 @@ const { promises: fsPromises } = fs;
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 console.log("contactsPath", contactsPath);
 
-// async function listContacts() {
+const hof = async function (youFunc) {
+  try {
+    const resultData = await fsPromises.readFile(contactsPath, "utf-8");
+    youFunc(resultData);
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+
+const listOfContactsInner = (resultData) => {
+  const list = JSON.parse(resultData);
+  console.log(
+    "list",
+    list.map((contact) => contact.name)
+  );
+};
+hof(listOfContactsInner());
+
+// const listContactsFunc = async function listContacts() {
+//     try {
+
+//       const resultData = await fsPromises.readFile(contactsPath, "utf-8");
+//       const list = JSON.parse(resultData);
+//       console.log(
+//         "list",
+//         list.map((contact) => contact.name)
+//       );
+
+//     } catch (err) {
+//       console.log("err", err);
+//     }
+//   };
+
+// const listContactsFunc = async function listContacts() {
 //   try {
 //     const resultData = await fsPromises.readFile(contactsPath, "utf-8");
 //     const list = JSON.parse(resultData);
@@ -16,9 +49,10 @@ console.log("contactsPath", contactsPath);
 //   } catch (err) {
 //     console.log("err", err);
 //   }
-// }
+// };
+
 // listContacts();
-// async function getContactById(contactId) {
+// const getContactByIdFunc = async function getContactById(contactId) {
 //   try {
 //     const result = await fsPromises.readFile(contactsPath, "utf-8");
 //     const contact = JSON.parse(result).find(
@@ -28,9 +62,9 @@ console.log("contactsPath", contactsPath);
 //   } catch (err) {
 //     console.log("err", err);
 //   }
-// }
-// getContactById(6);
-// async function removeContact(contactId) {
+// };
+// // getContactById(6);
+// const removeContactFunc = async function removeContact(contactId) {
 //   try {
 //     const result = await fsPromises.readFile(contactsPath, "utf-8");
 //     const filteredContacts = JSON.parse(result).filter(
@@ -40,26 +74,32 @@ console.log("contactsPath", contactsPath);
 //   } catch (err) {
 //     console.log("err", err);
 //   }
-// }
-// removeContact(5);
-// async function addContact(name, email, phone) {
+// };
+// // removeContact(5);
+// const addContactFunc = async function addContact(name, email, phone) {
 //   try {
 //     const result = await fsPromises.readFile(contactsPath, "utf-8");
 //     const currentData = JSON.parse(result);
-
-//     const addedContacts = currentData.push({
-//       id: 13,
-//       name: name,
-//       email: email,
-//       phone: phone,
-//     });
-//     const resultFinished = await fsPromises.writeFile(
-//       contactsPath,
-//       addedContacts
-//     );
-//     console.log("resultFinished", resultFinished);
+//     console.log("currentData", typeof currentData);
+//     const addedContacts = [
+//       ...currentData,
+//       {
+//         id: currentData.length + 1,
+//         name: name,
+//         email: email,
+//         phone: phone,
+//       },
+//     ];
+//     console.log("addedContacts", addedContacts);
+//     await fsPromises.writeFile(contactsPath, JSON.stringify(addedContacts));
 //   } catch (err) {
 //     console.log("err", err);
 //   }
-// }
+// };
 // addContact("a", "b", "c");
+module.exports = {
+  listContactsFunc,
+  // getContactByIdFunc,
+  // removeContactFunc,
+  // addContactFunc,
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+const mod = require("./contacts");
+mod.listContactsFunc();
+// mod.getContactByIdFunc(5);
+// mod.removeContactFunc(6);
+// mod.addContactFunc("hello", "world", "again");
