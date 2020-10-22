f71be5ae6e88303b89ea7d0c165560de4a930160
Anastasia-Dmv <Vesta50000@gmail.com>
2020-10-22 17:08:06 +0300

    refactoring

--- contacts.js
+++ contacts.js
@@ -3,74 +3,15 @@ const path = require("path");
 const { promises: fsPromises } = fs;
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
-console.log("contactsPath", contactsPath);
-//=============================
-async function errorCatchingFunction(youFunc) {
+
+async function errorHandlingWrapper(innerFn) {
   try {
     const resultData = await fsPromises.readFile(contactsPath, "utf-8");
     const list = JSON.parse(resultData);
-    return youFunc(list);
+    return innerFn(list);
   } catch (err) {
     console.log("err", err);
   }
 }
-//=====================
-const listOfContacts = (list) => {
-  console.log(
-    "listOfContacts",
-    //list
-    list.map((contact) => contact.name)
-  );
-};
 
-
-//==================
-const getContactById = (contactId) => (list) => {
-  const contact = list.find((item) => item.id === contactId);
-  console.log("getContactById", contact);
-};
-
-
-
-//=============================
-
-const removeContact = (contactId) => async (list) => {
-  const contacts = list.filter((contact) => contact.id !== contactId);
-  console.log(`removeContact ${contactId}`, contacts);
-  await fsPromises.writeFile(contactsPath, JSON.stringify(contacts));
-};
-
-
-//==================
-const addContact = (name, email, phone) => async (list) => {
-  const addedContacts = [
-    ...list,
-    {
-      id: list.length + 1,
-      name: name,
-      email: email,
-      phone: phone,
-    },
-  ];
-  console.log("addContact", addedContacts);
-  await fsPromises.writeFile(contactsPath, JSON.stringify(addedContacts));
-};
-
-
-const getListOfContacts = errorCatchingFunction(listOfContacts)
-// const getSomeContactById(id) = errorCatchingFunction(getContactById(id));
-// const removeSomeContact = errorCatchingFunction(removeContact);
-// const addSomeContact =  errorCatchingFunction(addContact);
-module.exports = {
-  getListOfContacts,
-  // getSomeContactById,
-  // removeSomeContact,
-  // addSomeContact,
-  errorCatchingFunction, 
-  getContactById,
-  removeContact, 
-  addContact
-
- 
-};
-
+module.exports = { errorHandlingWrapper };
--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-const {
-  getListOfContacts,
-  // getSomeContactById,
-  // removeSomeContact,
-  // addSomeContact,
-  errorCatchingFunction, 
-  getContactById,
-  removeContact, 
-  addContact
-  
-} = require("./contacts");
-
 const argv = require("yargs").argv;
+const fs = require("fs");
+const path = require("path");
+const { promises: fsPromises } = fs;
+const { errorHandlingWrapper } = require("./contacts");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const listOfContacts = (list) => {
+  console.log(
+    "listOfContacts",
+
+    list.map((contact) => contact.name)
+  );
+};
+const getListOfContacts = errorHandlingWrapper(listOfContacts);
+
+const getContactById = (contactId) => (list) => {
+  const contact = list.find((item) => item.id === contactId);
+  console.log("getContactById", contact);
+};
+
+const removeContact = (contactId) => async (list) => {
+  const contacts = list.filter((contact) => contact.id !== contactId);
+  console.log(`removeContact ${contactId}`, contacts);
+  await fsPromises.writeFile(contactsPath, JSON.stringify(contacts));
+};
+
+const addContact = (name, email, phone) => async (list) => {
+  const addedContacts = [
+    ...list,
+    {
+      id: list.length + 1,
+      name: name,
+      email: email,
+      phone: phone,
+    },
+  ];
+  console.log("addContact", addedContacts);
+  await fsPromises.writeFile(contactsPath, JSON.stringify(addedContacts));
+};
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
@@ -19,18 +47,18 @@ function invokeAction({ action, id, name, email, phone }) {
       break;
 
     case "get":
-      errorCatchingFunction(getContactById(id))
-      // getSomeContactById(id);
+      errorHandlingWrapper(getContactById(id));
+
       break;
 
     case "add":
-      errorCatchingFunction(addContact(name, email, phone))
-      //removeSomeContact(id);
+      errorHandlingWrapper(addContact(name, email, phone));
+
       break;
 
     case "remove":
-      errorCatchingFunction(removeContact(id))
-      // addSomeContact(name, email, phone);
+      errorHandlingWrapper(removeContact(id));
+
       break;
 
     default:
