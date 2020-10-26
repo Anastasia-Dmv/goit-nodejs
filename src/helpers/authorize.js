cf70d3432c76086104d964b57f18f19b5d5037ef
Anastasia-Dmv <Vesta50000@gmail.com>
2020-10-26 18:25:50 +0200

    verify token

--- src/helpers/authorize.js
+++ src/helpers/authorize.js
@@ -1,4 +1,5 @@
 // const { required } = require("joi");
+//const jwt = require("jsonwebtoken");
 const jwt = require("jsonwebtoken");
 //const jwt = require("jsonwebtoken");
 const { UserModel } = require("../users/users.model");
@@ -6,8 +7,13 @@ const { Unauthorized } = require("./errors/Unauthorized.error");
 
 exports.authorize = async (req, res, next) => {
   try {
-    const { token } = req.cookies;
+    console.log("req.headers", req.headers);
+    const token = req.cookies;
+    //const newToken = req.headers.authorization.split(" ")[1];
+    console.log("token==========>", token);
+    console.log("process.env.JWT_SECRET", process.env.JWT_SECRET);
     const payload = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("payload", payload);
     const user = await UserModel.findById(payload.userId);
     if (!user) {
       throw new Unauthorized();
