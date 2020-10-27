eee18354b53b60c80ca3d7eab3232ac4dab8c457
Anastasia-Dmv <Vesta50000@gmail.com>
2020-10-27 17:51:27 +0200

    update user

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,6 +1,42 @@
+const { UserModel } = require("./users.model");
+
 exports.getCurrentUser = (req, res, next) => {
   return res.status(200).send({
     id: req.user._id,
     email: req.user.email,
   });
+};
+exports.updateCurrentUser = async (req, res, next) => {
+  console.log("req.params;", req.params);
+  const { contactId } = req.params;
+  const contact = await UserModel.findByIdAndUpdate(
+    JSON.parse(contactId),
+    req.body,
+    {
+      new: true,
+    }
+  );
+
+  if (!contact) {
+    return res.status(404).json("Not found");
+  }
+  return res.status(200).json("Contact was updated ");
+  //   console.log("req.body", req.body);
+  //   console.log(typeOf(req.body.id));
+  //   const user = await UserModel.findByIdAndUpdate(
+  //     req.body.id,
+  //     req.body.subscription,
+  //     {
+  //       new: true,
+  //     }
+  //   );
+  //   console.log("user", user);
+  //   if (!user) {
+  //     return res.status(404).json("Not found");
+  //   }
+  //   return res.status(200).send({
+  //     id: req.user._id,
+  //     email: req.user.email,
+  //     subscription: req.body.subscription,
+  //   });
 };
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const { authorize } = require("../helpers/authorize");
-const { getCurrentUser } = require("./users.controller");
+const { errorHandlingWrapper } = require("../helpers/utils");
+const { getCurrentUser, updateCurrentUser } = require("./users.controller");
 const router = Router();
 
-router.get("/current", authorize, getCurrentUser);
+router.get("/current", authorize, errorHandlingWrapper(getCurrentUser));
+router.patch("/users", authorize, errorHandlingWrapper(updateCurrentUser));
 
 exports.usersRouter = router;
